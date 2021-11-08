78cd711842efd83b06fa0b09c796e3e141ef5178
Zin <tyulkeds159357@gmail.com>
2021-11-08 23:43:05 +0800

    add restaurant and admin index page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
-module.exports = (app) => {
+const restController = require('../controllers/restController')
+const adminController = require('../controllers/adminController')
 
-  app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+module.exports = app => {
+  //使用者如訪問首頁就會導向 / restaurants 頁面
+  app.get('/', (req, res) => res.redirect('/restaurants'))
+  //在 /restaurant 底下交給 restController.getRestaurants 處理
+  app.get('/restaurants', restController.getRestaurants)
+  //連到 /admin 頁面轉到 /admin/restaurants
+  app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
+  //在 /admin/restaurants 底下交給 adminControlles.getRestaurants 處理
+  app.get('/admin/restaurants', adminController.getRestaurants)
 }
