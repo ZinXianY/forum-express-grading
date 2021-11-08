1d956d864271ae4de1a265a2302d240d7702be1a
Zin <tyulkeds159357@gmail.com>
2021-11-08 17:38:39 +0800

    Initialize project

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const exhbs = require('express-handlebars')
 const app = express()
 const port = 3000
+
+//設定樣板引擎
+app.engine('hbs', exhbs())
+app.set('view engine', 'hbs')
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
