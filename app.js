a81600efde6bb4a38d8048f36003aed8993d2fd6
Zin <tyulkeds159357@gmail.com>
2021-11-09 17:04:36 +0800

    Install express-session and connect-flash

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const db = require('./models') //引入資料庫
+const flash = require('connect-flash')
+const session = require('express-session')
 const app = express()
 const port = 3000
 
@@ -10,6 +12,16 @@ app.set('view engine', 'handlebars')
 
 //設定 body-parser
 app.use(express.urlencoded({ extended: true }))
+
+//設定 seesion, connect-flash
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+// req.flash 放入 res.locals
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
