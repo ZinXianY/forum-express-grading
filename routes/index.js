d454ab4db252cece84c7b8fc6520855e029dc7b7
Zin <tyulkeds159357@gmail.com>
2021-11-09 18:14:23 +0800

    Add authentication before rendering pages

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,34 @@ const adminController = require('../controllers/adminController')
 const userController = require('../controllers/userController')
 
 module.exports = (app, passport) => {
+  //加入身分驗證
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/signin')
+  }
+
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) { return next() }
+      return res.redirect('/')
+    }
+    res.redirect('/signin')
+  }
+
   //使用者如訪問首頁就會導向 / restaurants 頁面
-  app.get('/', (req, res) => res.redirect('/restaurants'))
+  app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
+
   //在 /restaurant 底下交給 restController.getRestaurants 處理
-  app.get('/restaurants', restController.getRestaurants)
+  app.get('/restaurants', authenticated, restController.getRestaurants)
+
   //連到 /admin 頁面轉到 /admin/restaurants
-  app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
+
   //在 /admin/restaurants 底下交給 adminControlles.getRestaurants 處理
-  app.get('/admin/restaurants', adminController.getRestaurants)
+  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
+
   //設定註冊路由
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
